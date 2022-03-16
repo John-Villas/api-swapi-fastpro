161787f355c36e39c39086b8267fda22a7d52336
John Villas <joaovictor.vbt@gmail.com>
2022-03-15 22:03:16 -0300

    fix: port for heroku

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { routes } from "./routes"
 import "./database"
 import cors from "cors"
 
-const port = 3003
+const port = process.env.PORT || 3003
 const app = express()
 
 app.use(express.json())
